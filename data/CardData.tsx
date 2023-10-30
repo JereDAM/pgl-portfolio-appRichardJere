021bee52d32a766b54039421ea2b0f92deeefc5b
rica07b <rica07b@gmail.com>
2023-10-30 19:36:15 +0000

    feat : añadimos info del card de ricardo

--- data/CardData.tsx
+++ data/CardData.tsx
@@ -1,26 +1,26 @@
 import { ImageProps } from "react-native";
 
 export type CardData = {
-  id: number,
-  image: ImageProps,
-  title: string,
-  body: string,
-  imageBackground: ImageProps
+  id: number;
+  image: ImageProps;
+  title: string;
+  body: string;
+  imageBackground: ImageProps;
 };
 
 export const cardData: CardData[] = [
   {
-    id:1,
-    image: require("./../assets/SofyanAmrabat.jpg"),
-    title: "¡Mi descripción!",
-    body: "Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos",
-    imageBackground:require("./../assets/jerebackground.webp")
+    id: 1,
+    image: require("./../assets/lee.jpg"),
+    title: "Ricardo Colleman",
+    body: "Soy un intento de culturista , al que le gusta reirse de las personas con alopecia degenerativa (es irónico porque yo tambien lo tengo)",
+    imageBackground: require("./../assets/goldo.jpg"),
   },
   {
-    id:2,
-    image:require("./../assets/SoyEse.png"),
-    title: 'Sobre mi',
-    body:"Soy alumno de 2ºDAM y espero poder llegar a poder desarrollar mi propio videojuego. \nある中国人はかつてこう言いました。チンギンガン、チーチュンギンガン、チュチュチアオチョ ",
-    imageBackground:require("./../assets/jerebackground2.jpg")
-  }
-]
+    id: 2,
+    image: require("./../assets/SoyEse.png"),
+    title: "Sobre mi",
+    body: "Soy alumno de 2ºDAM y espero poder llegar a poder desarrollar mi propio videojuego. \nある中国人はかつてこう言いました。チンギンガン、チーチュンギンガン、チュチュチアオチョ ",
+    imageBackground: require("./../assets/jerebackground2.jpg"),
+  },
+];
