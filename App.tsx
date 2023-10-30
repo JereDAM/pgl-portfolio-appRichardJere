556a4e7536ac904f93a77085283af8267baf2f07
rica07b <rica07b@gmail.com>
2023-10-30 19:32:43 +0000

    feat : se añadieron states para el modo oscuro

--- App.tsx
+++ App.tsx
@@ -5,11 +5,28 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 export default function App() {
+  const [isCheckIcon, setIsCheckIcon] = React.useState(true);
   const [displayMyQR, setDisplayMyQR] = useState(true);
+  const onClickIcon = () => {
+    setIsCheckIcon(false);
+  };
+
+  const onClickDarkIcon = () => {
+    setIsCheckIcon(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Header setDisplayMyQR={setDisplayMyQR} />
-      <Body displayTheQR={displayMyQR} />
+      <Header
+        setDisplayMyQR={setDisplayMyQR}
+        isCheckIcon={isCheckIcon}
+        onClickIcon={onClickIcon}
+        onClickDarkIcon={onClickDarkIcon}
+      />
+      <Body
+        displayTheQR={displayMyQR}
+        isCheckIcon={isCheckIcon}
+      />
     </View>
   );
 }
